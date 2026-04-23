c3d2f60e1a745f9b6b7bbc164f870480f7a85926
Lucas Silva <nathansg20@gmail.com>
2026-04-22 23:28:39 -0300

    Seller entity class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,5 +1,23 @@
 package Application;
 
+import java.text.SimpleDateFormat;
+import java.sql.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
+
+    private SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy");
+
+    public static void main(String[] args) {
+
+        Seller seller = new Seller(5, "Lucas", "Lucas@gmail.com", Date.valueOf("2000-07-20"), 3500.0, new Department(1, "Computadores"));
+
+        System.out.println(seller);
+
+
+
+    }
 
 }
